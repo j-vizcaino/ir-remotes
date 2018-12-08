921c582170e45297ee39ebf09a1e1ace499afd3d
Jérôme Vizcaino <jerome.vizcaino@gmail.com>
2018-12-08 23:04:02 +0100

    Add package to helper reading/writing JSON

---
+++ pkg/utils/json.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+func Save(obj interface{}, out io.Writer) error {
+	enc := json.NewEncoder(out)
+	enc.SetIndent("", "  ")
+	return enc.Encode(obj)
+}
+
+func SaveToFile(obj interface{}, filename string) error {
+
+	fd, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	return Save(obj, fd)
+}
+
+func Load(obj interface{}, in io.Reader) error {
+	dec := json.NewDecoder(in)
+	return dec.Decode(obj)
+}
+
+func LoadFromFile(obj interface{}, filename string) error {
+	fd, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+	return Load(obj, fd)
+}
