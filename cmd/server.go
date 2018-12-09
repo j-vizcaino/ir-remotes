b2eb20a1f9826c4fc9f0a2b637197cd6b0c4912a
Jérôme Vizcaino <jerome.vizcaino@gmail.com>
2018-12-09 01:41:32 +0100

    Factorize server code

--- cmd/server.go
+++ cmd/server.go
@@ -28,7 +28,13 @@ func init() {
 	cmdRoot.AddCommand(cmdServer)
 }
 
-func mustLoadDevices() (devices.DeviceInfoList, []broadlink.Device) {
+type Handler struct {
+	deviceInfoList devices.DeviceInfoList
+	deviceList []broadlink.Device
+	remoteList remotes.RemoteList
+}
+
+func mustHandler() *Handler {
 	devInfoList := devices.DeviceInfoList{}
 	if err := utils.LoadFromFile(&devInfoList, devicesFile); err != nil {
 		log.WithError(err).WithField("devices-file", devicesFile).Fatal("Failed to load devices from file.")
@@ -40,109 +46,107 @@ func mustLoadDevices() (devices.DeviceInfoList, []broadlink.Device) {
 	if err != nil {
 		log.WithError(err).Fatal("Failed to initialize a Broadlink device")
 	}
-	return devInfoList, devList
-}
 
-func mustLoadRemotes() remotes.RemoteList {
-	remotes := remotes.RemoteList{}
-	if err := utils.LoadFromFile(&remotes, remotesFile); err != nil {
+	remoteList := remotes.RemoteList{}
+	if err := utils.LoadFromFile(&remoteList, remotesFile); err != nil {
 		log.WithError(err).WithField("remotes-file", remotesFile).Fatal("Failed to load remotes from file")
 	}
-	if len(remotes) == 0 {
+	if len(remoteList) == 0 {
 		log.WithField("remotes-file", remotesFile).Fatal("No remote listed in file. Aborting.")
 	}
-	return remotes
+
+	return &Handler{
+		deviceInfoList: devInfoList,
+		deviceList: devList,
+		remoteList: remoteList,
+	}
 }
 
+func (h *Handler) abortNotFound(c *gin.Context, err string) {
+	h.abort(c, http.StatusNotFound, err)
+}
+
+func (h *Handler) abort(c *gin.Context, code int, err string) {
+	c.AbortWithStatusJSON(
+		code,
+		gin.H{
+			"success": false,
+			"error": err,
+		},
+	)
+}
+
+func (h *Handler) getDevices(c *gin.Context) {
+	c.JSON(http.StatusOK, h.deviceInfoList)
+}
+
+func (h *Handler) getDevice(c *gin.Context) {
+	devName := c.Param("device")
+	devInfo, found := h.deviceInfoList.Find(func(d devices.DeviceInfo) bool {
+		return d.Name == devName
+	})
+	if !found {
+		h.abortNotFound(c, fmt.Sprintf("no such device named %q", devName))
+		return
+	}
+	c.JSON(http.StatusOK, devInfo)
+}
+
+func (h *Handler) getRemotes(c *gin.Context) {
+	c.JSON(http.StatusOK, h.remoteList.Names())
+}
+
+func (h *Handler) helperGetRemote(c *gin.Context) *remotes.Remote {
+	name := c.Param("remote")
+	remote := h.remoteList.Find(name)
+	if remote == nil {
+		h.abortNotFound(c, fmt.Sprintf("no such remote named %q", name))
+	}
+	return remote
+}
+
+func (h *Handler) getRemote(c *gin.Context) {
+	r := h.helperGetRemote(c)
+	if r == nil {
+		return
+	}
+	c.JSON(http.StatusOK, r)
+}
+
+func (h *Handler) postRemoteCommand(c *gin.Context) {
+	remote := h.helperGetRemote(c)
+	if remote == nil {
+		return
+	}
+
+	name := c.Param("command")
+	cmd, ok := remote.Commands[name]
+	if !ok {
+		h.abortNotFound(c, fmt.Sprintf("remote %q has no command %q", remote.Name, name))
+		return
+	}
+	dev := h.deviceList[0]
+	if err := dev.SendIRRemoteCode(cmd, 1); err != nil {
+		h.abort(c, http.StatusInternalServerError, fmt.Sprintf("send IR command failed: %s", err))
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"success": true})
+}
+
+
 func Server(_ *cobra.Command, _ []string) {
-	devInfoList, deviceList := mustLoadDevices()
-	remoteList := mustLoadRemotes()
+	h := mustHandler()
 
 	r := gin.Default()
 
 	api := r.Group("/api")
-	api.GET("/devices/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, devInfoList)
-	})
-
-	api.GET("/devices/:device", func(c *gin.Context) {
-		devName := c.Param("device")
-		devInfo, found := devInfoList.Find(func(d devices.DeviceInfo) bool {
-			return d.Name == devName
-		})
-		if !found {
-			c.AbortWithStatusJSON(
-				http.StatusNotFound,
-				gin.H{
-					"success": false,
-					"error": fmt.Sprintf("no such device named %q", devName),
-				},
-			)
-			return
-		}
-		c.JSON(http.StatusOK, devInfo)
-	})
-
-	api.GET("/remotes/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, remoteList.Names())
-	})
-
-	api.GET("/remotes/:remote", func(c *gin.Context) {
-		name := c.Param("remote")
-		remote := remoteList.Find(name)
-		if remote == nil {
-			c.AbortWithStatusJSON(
-				http.StatusNotFound,
-				gin.H{
-					"success": false,
-					"error": fmt.Sprintf("no such remote named %q", name),
-				},
-			)
-			return
-		}
-		c.JSON(http.StatusOK, remote)
-	})
-
+	api.GET("/devices/", h.getDevices)
+	api.GET("/devices/:device", h.getDevice)
+	api.GET("/remotes/", h.getRemotes)
+	api.GET("/remotes/:remote", h.getRemote)
+	api.POST("/remotes/:remote/:command", h.postRemoteCommand)
 	api.GET("/remotes/:remote/:command", func(c *gin.Context) {
 		c.AbortWithStatus(http.StatusMethodNotAllowed)
-	})
-	api.POST("/remotes/:remote/:command", func(c *gin.Context) {
-		name := c.Param("remote")
-		remote := remoteList.Find(name)
-		if remote == nil {
-			c.AbortWithStatusJSON(
-				http.StatusNotFound,
-				gin.H{
-					"success": false,
-					"error": fmt.Sprintf("no such remote named %q", name),
-				},
-			)
-			return
-		}
-		name = c.Param("command")
-		cmd, ok := remote.Commands[name]
-		if !ok {
-			c.AbortWithStatusJSON(
-				http.StatusNotFound,
-				gin.H{
-					"success": false,
-					"error": fmt.Sprintf("remote %q has no command %q", remote.Name, name),
-				},
-			)
-			return
-		}
-		dev := deviceList[0]
-		if err := dev.SendIRRemoteCode(cmd, 1); err != nil {
-			c.AbortWithStatusJSON(
-				http.StatusInternalServerError,
-				gin.H{
-					"success": false,
-					"error": fmt.Sprintf("send IR command failed: %s", err),
-				},
-			)
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"success": true})
 	})
 
 	if err := r.Run(listenAddress); err != nil {
